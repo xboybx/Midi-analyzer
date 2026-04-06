dafee616c8190310a93d41d385fcdb3de95b1e50
username-xboybx <jeswanth012@gmail.com>
2026-04-06 16:35:22 +0530

    changd thefav icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,14 +16,8 @@ export const metadata: Metadata = {
   title: "Chord Analyzer",
   description: "Midi detector and Chord analyzer",
   icons: {
-    icon: [
-      { url: "/126.png", sizes: "126x126" },
-      { url: "/168.png", sizes: "168x168" },
-      { url: "/512.png", sizes: "512x512" },
-    ],
-    apple: [
-      { url: "/512.png", sizes: "512x512" },
-    ],
+    icon: "/126.png",
+    apple: "/512.png",
   },
 };
 
